9120dd46b3f1dad3c8a8697aff1eb4406d1b060e
GabriellMartins <gabrielnpmartins@hotmail.com>
2024-11-09 05:08:24 -0300

    add RewardService

--- src/main/java/com/br/gabrielmartins/command/PaymentsCommand.java
+++ src/main/java/com/br/gabrielmartins/command/PaymentsCommand.java
@@ -7,11 +7,11 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class BRPaymentsCommand implements CommandExecutor {
+public class PaymentsCommand implements CommandExecutor {
 
     private final BukkitMain plugin;
 
-    public BRPaymentsCommand(BukkitMain plugin) {
+    public PaymentsCommand(BukkitMain plugin) {
         this.plugin = plugin;
     }
 
